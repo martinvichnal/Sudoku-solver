d7ec8183c44832991ea946faa4f97b4671443da4
vmazser <vmazsit@gmail.com>
2022-01-20 18:12:32 +0100

    Opening

--- SudokuTester/Program.cs
+++ SudokuTester/Program.cs
@@ -11,7 +11,7 @@ namespace SudokuTester
         // 4 by 4
         // Test numbers => 2143,4321,1234.3412 .. 1234,4312,2143,3421 .. 4312,2134,3421,1243 .. 3412,1243,2134,4321
         public static int[,] testNumber1 = { { 3, 4, 1, 2 }, { 1, 2, 4, 3 }, { 2, 1, 3, 4 }, { 4, 3, 2, 1 } };      // Test 2D Matrix Numbers 
-        public static int[,] testNumber2 = { { 2, 1, 4, 3 }, { 4, 3, 2, 1 }, { 1, 2, 3, 4 }, { 3, 4, 1, 2 } };      // Test 2D Matrix Numbers
+        public static int[,] testNumber2 = { { 2, 1, 4, 3 }, { 4, 4, 2, 1 }, { 1, 2, 3, 4 }, { 3, 4, 1, 2 } };      // Test 2D Matrix Numbers
         public static int[,] testNumber3 = { { 1, 2, 3, 4 }, { 4, 3, 1, 2 }, { 2, 1, 4, 3 }, { 3, 4, 2, 1 } };      // Test 2D Matrix Numbers
         public static int[,] testNumber4 = { { 4, 3, 1, 2 }, { 2, 1, 3, 4 }, { 3, 4, 2, 1 }, { 1, 2, 4, 3 } };      // Test 2D Matrix Numbers
 
@@ -38,7 +38,7 @@ namespace SudokuTester
                 for (int y = 0; y < 4; y++)
                 {
                     //Console.WriteLine($"O({x},{y})\t placed on {Sudoku.GetNumer(x, y)}\t and has a value of {Sudoku.sudokuNumbers[Sudoku.GetNumer(x, y) - 1]}\t is  {Sudoku.CheckingForRight(x, y)}\t");
-                    Console.WriteLine($"O({x},{y})\t placed on ...\t and has a value of {testNumber1[x, y]}\t is {Sudoku.CheckingForRight(x, y)}\t");
+                    Console.WriteLine($"O({x},{y})\t placed on ...\t and has a value of {testNumber2[x, y]}\t is {Sudoku.CheckingForRight(x, y)}\t");
 
                 }
             }
