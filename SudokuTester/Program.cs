49339b29ef20487486d77e1e8c5d66756008ca3d
vmazser <vmazsit@gmail.com>
2022-02-06 11:40:09 +0100

    Major Changes

--- SudokuTester/Program.cs
+++ SudokuTester/Program.cs
@@ -8,13 +8,29 @@ namespace SudokuTester
 {
     class Program
     {
-        // 4 by 4
-        // Test numbers => 2143,4321,1234.3412 .. 1234,4312,2143,3421 .. 4312,2134,3421,1243 .. 3412,1243,2134,4321
-        public static int[,] testNumber1 = { { 3, 4, 1, 2 }, { 1, 2, 4, 3 }, { 2, 1, 3, 4 }, { 4, 3, 2, 1 } };      // Test 2D Matrix Numbers 
-        public static int[,] testNumber2 = { { 2, 1, 4, 3 }, { 4, 4, 2, 1 }, { 1, 2, 3, 4 }, { 3, 4, 1, 2 } };      // Test 2D Matrix Numbers
-        public static int[,] testNumber3 = { { 1, 2, 3, 4 }, { 4, 3, 1, 2 }, { 2, 1, 4, 3 }, { 3, 4, 2, 1 } };      // Test 2D Matrix Numbers
-        public static int[,] testNumber4 = { { 4, 3, 1, 2 }, { 2, 1, 3, 4 }, { 3, 4, 2, 1 }, { 1, 2, 4, 3 } };      // Test 2D Matrix Numbers
+        // 4 by 4 Sudokus
+        // Test numbers => 3412,1243,2134,4321 .. 2143,4321,1234.3412 .. 1234,4312,2143,3421 .. 4312,2134,3421,1243
 
+        public static int[,] testNumber1 =      // Test 1 2D Matrix Numbers 
+        {
+            { 3, 4, 1, 2 },
+            { 1, 2, 4, 3 },
+            { 2, 1, 3, 4 },
+            { 4, 3, 2, 1 }
+        };
+
+        public static int[,] testNumber10 =     // Test 1 2D Matrix Numbers WITH 0
+        {
+            { 0, 4, 1, 0 },
+            { 1, 0, 4, 3 },
+            { 2, 1, 0, 4 },
+            { 0, 3, 2, 1 }
+        };      
+
+        //public static int[,] testNumber2 = { { 2, 1, 4, 3 }, { 4, 3, 2, 1 }, { 1, 2, 3, 4 }, { 3, 4, 1, 2 } };      // Test 2 2D Matrix Numbers
+        //public static int[,] testNumber2w = { { 2, 1, 4, 3 }, { 4, 4, 2, 1 }, { 1, 2, 3, 4 }, { 3, 4, 1, 2 } };      // Test 2 2D Matrix Numbers WRONG
+        //public static int[,] testNumber3 = { { 1, 2, 3, 4 }, { 4, 3, 1, 2 }, { 2, 1, 4, 3 }, { 3, 4, 2, 1 } };      // Test 3 2D Matrix Numbers
+        //public static int[,] testNumber4 = { { 4, 3, 1, 2 }, { 2, 1, 3, 4 }, { 3, 4, 2, 1 }, { 1, 2, 4, 3 } };      // Test 4 2D Matrix Numbers
         static void Main(string[] args)
         {
             Console.WriteLine("Hello this is my sudoku tester program where I testing each methods");
@@ -22,24 +38,12 @@ namespace SudokuTester
             //var testNumberArrays = new[] { testNumber1, testNumber2, testNumber3, testNumber4 };
 
             Sudoku.gridSize = testNumber1.Length;
-
-            //int x = 0;
-            //int y = 0;
-            //while (true)
-            //{
-            //    Console.Write($"x: "); x = int.Parse(Console.ReadLine());
-            //    Console.Write($"y: "); y = int.Parse(Console.ReadLine());
-            //    Console.WriteLine($"O({x},{y})\t placed on {Sudoku.GetNumber(x, y)}\t and has a value of {testNumber1[x, y]}\t is  {Sudoku.CheckingForRight(x, y)}\t");
-            //}
-
-
             for (int x = 0; x < 4; x++)
             {
                 for (int y = 0; y < 4; y++)
                 {
-                    //Console.WriteLine($"O({x},{y})\t placed on {Sudoku.GetNumer(x, y)}\t and has a value of {Sudoku.sudokuNumbers[Sudoku.GetNumer(x, y) - 1]}\t is  {Sudoku.CheckingForRight(x, y)}\t");
-                    Console.WriteLine($"O({x},{y})\t placed on ...\t and has a value of {testNumber2[x, y]}\t is {Sudoku.CheckingForRight(x, y)}\t");
 
+                    Console.WriteLine($"O({x},{y})\t placed on ...\t and has a value of {testNumber10[x, y]}\t is {Sudoku.CheckingForRight(x, y)}\t");
                 }
             }
 
--- SudokuTester/Sudoku.cs
+++ SudokuTester/Sudoku.cs
@@ -9,44 +9,39 @@ namespace SudokuTester
     class Sudoku
     {
         // Public variables for the program
-        //public static List<int> sudokuNumbers = new List<int>();
 
-        public static int sqrtGrid { get; set; }        // The Squareroot for the gridSize
-        public static int gridSize { get; set; }        // Grid Size
+        // public static List<int> sudokuNumbers = new List<int>();
+        public static int sqrtGrid { get; set; }                    // The Squareroot for the gridSize
+        public static int gridSize { get; set; }                    // Grid Size
 
         public static int[,] problemCoordiantes { get; set; }       // The coordiantes for the wrong numbers
 
 
 
-        // This now is basically useless ... NOICE ... :)
         // Main Calculation for Getting the Serial number of a selected square.
         public static int GetNumber(int x, int y)
         {
             // Serial number = (x - 1) * gridCount(for exaplme 4*4) + y
-
             sqrtGrid = Convert.ToInt32(Math.Sqrt(gridSize));
-
-            int number = (x - 1) * sqrtGrid + y;
-
-            return number;
+            return (x - 1) * sqrtGrid + y;
         }
 
 
         // Main Method for checking the Vertical and the Horizontal numbers for any duplicants
+        // Note: This can be maybe faster if the vertical and the horizontal is checked individually 
+        //       i.e: 4x4 -> 4 horizontal and 4 vertical check. Wrong number is saved.
+
+
         public static bool CheckingForRight(int x, int y)
         {
             sqrtGrid = Convert.ToInt32(Math.Sqrt(gridSize));
-
             int problemCount = 0;
-
             bool isFoundProblem = false;
 
             ////////////////////////////////////// ****** VERTICAL ****** //////////////////////////////////////
-            // Chekcing for VERTICAL problems   //
-            // Adding 4 every upper checks      //
-            // Removing 4 every down checks     //
-            //////////////////////////////////////
-
+            //   Chekcing for VERTICAL problems : Adding 4 every upper checks, removing 4 every down checks   //
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+            /// The same number can't be in the vertical line
             int yUpper = y;
             int yLower = y;
             bool isThereSomethingVertical = true;
@@ -55,48 +50,43 @@ namespace SudokuTester
             while (isThereSomethingVertical)
             {
                 yUpper = yUpper - 1;
+                // If it's out of bound it will brake from the while cycle 
                 if (yUpper < 0)
                 {
-                    isThereSomethingVertical = true;
                     break;
                 }
-                else
+                // Checking if is equals to it's upper value
+                if (Program.testNumber10[x, y] == Program.testNumber10[x, yUpper] && Program.testNumber10[x, yUpper] != 0)
                 {
-                    if (Program.testNumber1[x, y] == Program.testNumber1[x, yUpper])
-                    {
-                        problemCount++;
-                        isFoundProblem = true;
-                    }
+                    problemCount++;
+                    isFoundProblem = true;
+                    bool isFoundProblemVertically = true;
                 }
             }
 
-            isThereSomethingVertical = true;
-
             // Checking the lower numbers
+            isThereSomethingVertical = true;            // Reseting the bool value for the lower numbers
             while (isThereSomethingVertical)
             {
                 yLower = yLower + 1;
+                // If it's out of bound it will brake from the while cycle 
                 if (yLower > sqrtGrid - 1)
-                {
-                    isThereSomethingVertical = true;
+                { 
                     break;
                 }
-                else
+                // Checking if is equals to it's lower value
+                if (Program.testNumber10[x, y] == Program.testNumber10[x, yLower] && Program.testNumber10[x, yLower] != 0)
                 {
-                    if (Program.testNumber1[x, y] == Program.testNumber1[x, yLower])
-                    {
-                        problemCount++;
-                        isFoundProblem = true;
-                    }
+                    problemCount++;
+                    isFoundProblem = true;
+                    bool isFoundProblemVertically = true;
                 }
             }
 
             ////////////////////////////////////// ****** HORIRZONTAL ****** //////////////////////////////////////
-            // Chekcing for HORIRZONTAL problems//
-            // Adding 1 every upper checks      //
-            // Removing 1 every left checks     //
-            //////////////////////////////////////
-
+            //   Chekcing for HORIRZONTAL problems : Adding 1 every upper checks, removing 1 every left checks   //
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////
+            /// The same number can't be in the horizontal line
             int xRight = x;
             int xLeft = x;
             bool isThereSomethingHorizontal = true;
@@ -105,39 +95,53 @@ namespace SudokuTester
             while (isThereSomethingHorizontal)
             {
                 xRight++;
+                // If it's out of bound it will brake from the while cycle 
                 if (xRight > sqrtGrid - 1)
                 {
-                    isThereSomethingHorizontal = true;
                     break;
                 }
-                else
+                // Checking if is equals to it's right value
+                if (Program.testNumber10[x, y] == Program.testNumber10[xRight, y] && Program.testNumber10[xRight, y] != 0)
                 {
-                    if (Program.testNumber1[x, y] == Program.testNumber1[xRight, y])
-                    {
-                        problemCount++;
-                        isFoundProblem = true;
-                    }
+                    problemCount++;
+                    isFoundProblem = true;
+                    bool isFoundProblemHorizontally = true;
                 }
             }
 
             // Checking the Left side numbers
+            isThereSomethingHorizontal = true;          // Reseting the bool value for the left numbers
             while (isThereSomethingHorizontal)
             {
                 xLeft--;
                 if (xLeft < 0)
                 {
-                    isThereSomethingHorizontal = true;
                     break;
                 }
-                else
+                // Checking if is equals to it's left value
+                if (Program.testNumber10[x, y] == Program.testNumber10[xLeft, y] && Program.testNumber10[xLeft, y] != 0)
                 {
-                    if (Program.testNumber1[x, y] == Program.testNumber1[xLeft, y])
-                    {
-                        problemCount++;
-                        isFoundProblem = true;
-                    }
+                    problemCount++;
+                    isFoundProblem = true;
+                    bool isFoundProblemHorizontally = true;
                 }
             }
+
+
+            ////////////////////////////////////// ****** SMALL BOX CHECK ****** //////////////////////////////////////
+            //                                   Chekcing problems in the small box                                  //
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            int smallBoxSize = Convert.ToInt32(Math.Sqrt(sqrtGrid));
+
+
+
+
+
+
+            // Storeing the coordinates for the problematical number
+
+            // Returning a bool if there is a problem or not. (false = no prblem, true = problem)
             return isFoundProblem;
         }
     }
